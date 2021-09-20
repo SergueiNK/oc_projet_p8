984a06ee460df3dae63b45a1269912b5c2c4de23
SergueiNK <nikolaev.serguei777@gmail.com>
2021-09-20 12:04:33 +0200

    login and register done now need to finish the the front pages

--- purbeurre/purbeurre_project/user/urls.py
+++ purbeurre/purbeurre_project/user/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user, name='user'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
+    path('login', views.loginPage, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('register', views.registerPage, name='register'),
 ]
--- purbeurre/purbeurre_project/user/views.py
+++ purbeurre/purbeurre_project/user/views.py
@@ -1,23 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
 
 # Create your views here.
 def user(request):
     return render(request, 'user/user.html')
 
-def register(request):
+def registerPage(request):
     form = CreateUserForm()
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Le compte a été crée pour' + user)
+            return redirect('user')
+        else:
+            messages.info(request, 'le le le')    
 
     context = {'form':form}
     return render(request, 'user/register.html', context)
 
-def login(request):
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('user')
+        else:
+            messages.info(request, 'Le nom d\'utilisateur OU le mot de passe sont incorrectes')
+
     context = {}
     return render(request, 'user/login.html', context)
+
+# Need to finalise the navbar
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
