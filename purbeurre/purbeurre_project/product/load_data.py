9c089e5c11f05767f02d0e7db90f118844dfc73e
SergueiNK <nikolaev.serguei777@gmail.com>
2021-10-07 22:53:24 +0200

    take data and clean data is ok, need to do models and resolve the problem with requests

--- purbeurre/purbeurre_project/product/load_data.py
+++ purbeurre/purbeurre_project/product/load_data.py
@@ -1,4 +1,8 @@
-import request
+#!/usr/bin/python3.9
+# -*- coding:utf-8 -*-
+
+import requests
+import json
 
 url_request = 'https://fr.openfoodfacts.org/cgi/search.pl'
 products_params = {
@@ -8,21 +12,20 @@ products_params = {
     "json": 1,
     "page": 1,
     "fields": "pnns_groups_1,generic_name_fr,"
-              "code,url,nutrition_grade_fr,stores"
+              "code,url,nutrition_grade_fr,image_url,image_nutrition_url"
 }
 
+# get API
 def api_get_products():
+    """Launch the API fetch"""
     try:
-        request_response = request.get(url_request, products_params)
-        response = json.loads(request_response.txt)
+        request_response = requests.get(url_request, products_params)
+        #print(request_response)
+        #print(request_response.url)
+        response = json.loads(request_response.text)
+        #print(response)
         return response.get('products')
-        print('products')
     except Exception as e:
         raise e
 
-if __name__ == "__main__":
-    try:
-        create_database()
-        print("Base de données créée")
-    except Exception as e:
-        print(e)
+
--- purbeurre/purbeurre_project/product/views.py
+++ purbeurre/purbeurre_project/product/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def product(request):
     return render(request, 'product/product.html')
-# Create your views here.
+
