de6efe2f6c5b88278513a235bd9b8f55c20801e9
SergueiNK <nikolaev.serguei777@gmail.com>
2021-10-13 21:45:09 +0200

    the first models is ok

--- purbeurre/purbeurre_project/product/admin.py
+++ purbeurre/purbeurre_project/product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
+# Pour pouvoir modifier les données de register depuis Product
+# Dépuis admin ça permet d'ajouter du site ça permet de faire les modifications
+admin.site.register(Product)
 # Register your models here.
