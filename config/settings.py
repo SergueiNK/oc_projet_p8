255ecab1c945601af61064e9236cb39eb3469f55
SergueiNK <nikolaev.serguei777@gmail.com>
2021-12-24 19:33:33 +0100

    add modification to allowed_hosts to purbeurre-spatial.herokuapps.com

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wfon4(q*m$=j^^wxpy-6mdb*xuma=xwud&)wp8d=tdz7j5-ou^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('ENV', 'development') == 'production' else True
 
-ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['purbeurre-spatial.herokuapps.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
