61cb94e9dd8756294683ca6daa35b6813071b1df
SergueiNK <nikolaev.serguei777@gmail.com>
2021-12-24 19:50:23 +0100

    add modifications on staticfiles_dirs 3

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/dist'),
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/dist')
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
